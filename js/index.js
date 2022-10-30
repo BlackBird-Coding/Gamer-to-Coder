09aea2e6d0132f35b92b45da0cda31dd20591cd7
Bla_ckB <50193121+BlackBird-Coding@users.noreply.github.com>
2022-10-30 23:23:14 +0700

    update page

--- js/index.js
+++ js/index.js
@@ -4,13 +4,19 @@ const miniGames = await getMiniGames();
 const app = document.querySelector("#app");
 
 miniGames.map((miniGame) => {
+  let tag = "";
+
+  miniGame.genre.map((genre) => {
+    tag += `<div class="genre">${genre}</div>`;
+  });
   const e = document.createElement("div");
   e.className = "card";
-  e.innerHTML = `<image class="logo" src="${miniGame.icon}" /><div>
+  e.innerHTML = `<img class="logo" src="${miniGame.icon}" /><div>
     <p>${miniGame.name}</p>
-    <div class="description">
-      ${miniGame.genre.map((genre) => genre)}
+    <div class="tags">
+      ${tag}
     </div>
 </div>`;
+
   app.querySelector("#minigames").appendChild(e);
 });
